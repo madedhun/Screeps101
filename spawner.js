bcb94973aed263a78a542e8e84f1ae6020f51ba1
madedhun <papp.dani89@gmail.com>
2018-09-26 15:43:15 +0200

    Create spawner.js

---
+++ spawner.js
@@ -0,0 +1,27 @@
+require('class');
+module.exports = {
+
+	run: function()
+	{
+		
+		var newname = generatename('Creep');
+		var bodymod = ['WORK','MOVE','CARRY'];
+		//csak akkor spawnoljunk ha szükséges
+		for (let i in Game.spawns)
+		{
+			Game.spawns[i].Spawncreep(bodymod,newname);
+
+		}
+
+//a memóroiájába írjuk be a funkcióját
+
+
+
+	}
+
+
+
+
+//név generátor  generatename
+	
+};
