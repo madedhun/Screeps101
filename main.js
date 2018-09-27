78c2379efdf496a7e664108c6337962b5897add9
madedhun <papp.dani89@gmail.com>
2018-09-27 16:25:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var _Spawner = require('spawner');
 module.exports.loop = function () {
 	
@@ -14,6 +15,7 @@ module.exports.loop = function () {
     {
         creep.moveTo(sources[0]);
     }
+
 
 
  	
